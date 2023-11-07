75e5113d6ec8bc79c4a143568fdff03abaefb994
Milos Njegovanovic <njegovanovicm@yahoo.com>
2023-11-07 19:53:21 +0100

    Comment input finished design

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -1,12 +1,23 @@
 import useFormattedDate from '../hooks/useFormattedDate'
-import './Container.module.css'
+import styles from './Container.module.css'
 
 const Container = () => {
   const formattedDate = useFormattedDate()
 
   return (
-    <div>
+    <div className={styles.flex_container}>
       <p>{formattedDate}</p>
+      <div className={styles.comment_holder}>
+        <label htmlFor='comment'>
+          <input
+            className={styles.text_input}
+            id='comment'
+            type='text'
+            placeholder='...type something'
+          />
+        </label>
+        <button className={styles.send}>Send</button>
+      </div>
     </div>
   )
 }
